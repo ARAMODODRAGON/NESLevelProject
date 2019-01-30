59c160b106c1182b74af07e97f3a5cd9a0cf1258
ARAMODODRAGON <domara2000@gmail.com>
2019-01-29 20:53:09 -0500

    Started the Entity class
    Entity class
    PowerUp class inherits from entity
    Each powerup has a class inherits from powerup

---
+++ Mario Bros 3 recreation/Assets/Prefabs/Entity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour {
+    //player variables
+    protected GameObject Player;
+    protected Transform PlayerTransform;
+
+    protected virtual void Start() {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerTransform = Player.transform;
+    }
+
+    protected virtual void FixedUpdate() {
+        //gets the distance to the player
+        Vector2 distanceToPlayer = transform.position - PlayerTransform.position;
+        //sets the x and y to their absolute values
+        distanceToPlayer.x = Mathf.Abs(distanceToPlayer.x);
+        distanceToPlayer.y = Mathf.Abs(distanceToPlayer.y);
+
+        Debug.Log(distanceToPlayer);
+        if (distanceToPlayer.x < 6.0f || distanceToPlayer.y < 6.0f) {
+            OnScreen();
+        } else {
+            OffScreen();
+        }
+    }
+
+    protected abstract void OnScreen();
+
+    protected abstract void OffScreen();
+}
--- Mario Bros 3 recreation/Assets/Prefabs/Mario/Player.cs
+++ Mario Bros 3 recreation/Assets/Prefabs/Mario/Player.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class Player : MonoBehaviour {
     //inputs
     Joystick Axis;
     Button Primary;
     Button Secondary;
 
     //bool for changing the facing direction. 
-    bool isFacingRight;
+    static bool isFacingRight;
 
     private void Start() {
         string[] JoyX = new string[3];
         string[] JoyY = new string[3];
         JoyX[0] = "DPadX";
         Axis = new Joystick(JoyX, JoyY);
+        isFacingRight = true;
     }
 
     private void CheckFacingDirection() {
 
+    }
+
+    public static bool IsFacingRight {
+        get {
+            return isFacingRight;
+        }
     }
 }
